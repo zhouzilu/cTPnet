0cc9a5d906c5dfadd53a8dd2f9e523635f6a3a1c
zhouzilu <zhouzilu@pennmedicine.upenn.edu>
2019-06-12 09:51:23 -0400

    update

--- R/data.R
+++ R/data.R
@@ -4,6 +4,8 @@
 #' \describe{
 #'   \item{gene_names}{list of gene names}
 #'   \item{gene_names_seurat3}{list of gene names in Seurat 3.0 format}
+#'   \item{cTPnet_demo}{data for demo, 11000 genes x 1000 cells from HCA}
 #' }
 "gene_names"
 "gene_names_seurat3"
+"cTPnet_demo"
